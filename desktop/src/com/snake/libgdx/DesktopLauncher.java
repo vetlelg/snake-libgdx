d8a43f820177a7c81469297af6281a35dd158bd6
vetlelg <61113128+vetlelg@users.noreply.github.com>
2022-09-06 17:06:52 +0200

    First commit

---
+++ desktop/src/com/snake/libgdx/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.snake.libgdx;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.snake.libgdx.Snake;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Snake libGDX");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
+		new Lwjgl3Application(new Snake(), config);
+	}
+}
